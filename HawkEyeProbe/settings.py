3ca3b4d24285203870afc93d59f20032448f132f
wangshu <wangshuchn@163.com>
2018-07-15 13:29:50 +0800

    删除默认数据库SQLlite

--- HawkEyeProbe/settings.py
+++ HawkEyeProbe/settings.py
@@ -74,10 +74,6 @@ WSGI_APPLICATION = 'HawkEyeProbe.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
