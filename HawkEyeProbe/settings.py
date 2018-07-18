6ea8f46d4db745cd5aa17e13a7621fe8e38113fb
root <wangshu@cecgw.cn>
2018-07-18 17:03:50 +0800

    Ubuntu update

--- HawkEyeProbe/settings.py
+++ HawkEyeProbe/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'HawkEye',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
