ad4d3bf2ba8c7561309f7f42fb0e1e3f7ad44ec4
root <wangshu@cecgw.cn>
2018-07-20 16:54:08 +0800

    modify "error" info.

--- HawkEyeProbe/settings.py
+++ HawkEyeProbe/settings.py
@@ -126,7 +126,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'HawkEye',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
